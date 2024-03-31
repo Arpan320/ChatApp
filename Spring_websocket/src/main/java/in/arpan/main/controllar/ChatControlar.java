ca3e5d4005214e605abe987326991fe1d7cd9715
Arpan320 <113209092+Arpan320@users.noreply.github.com>
2024-03-31 18:21:53 +0530

    Uploading 2nd time

---
+++ Spring_websocket/src/main/java/in/arpan/main/controllar/ChatControlar.java
@@ -0,0 +1,36 @@
+package in.arpan.main.controllar;
+
+import in.arpan.main.model.ChatMessage;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ChatControlar {
+	@MessageMapping("/chat.register")
+	@SendTo("/topic/public")
+	public ChatMessage register(@Payload  ChatMessage chatMessage, SimpMessageHeaderAccessor headerAccessor) {
+		headerAccessor.getSessionAttributes().put("username", chatMessage.getSender());
+		return chatMessage;
+	}
+	@MessageMapping("/chat.send")
+	@SendTo("/topic/public")
+	public ChatMessage sendMessage(@Payload ChatMessage chatMessage)
+	{
+		return chatMessage;
+	}
+	@MessageMapping("/chat.register1")
+	@SendTo("/topic/public1")
+	public ChatMessage register1(@Payload  ChatMessage chatMessage, SimpMessageHeaderAccessor headerAccessor) {
+		headerAccessor.getSessionAttributes().put("username", chatMessage.getSender());
+		return chatMessage;
+	}
+	@MessageMapping("/chat.send1")
+	@SendTo("/topic/public1")
+	public ChatMessage sendMessage2(@Payload ChatMessage chatMessage)
+	{
+		return chatMessage;
+	}
+}
